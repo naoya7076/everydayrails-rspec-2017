a6b1e67734424d2f376f4fb9a5550ca61fb48d10
naoya7076 <naoya7076@gmail.com>
2020-08-04 21:43:16 +0900

    projectのバリデーション

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  # ユーザー単位では重複したプロジェクト名を許可しないこと
-  it "does not allow duplicate project names per user" do
-    user = User.create(
+  before do
+    @user = User.create(
       first_name: "Naoya",
       last_name: "Shimizu",
       email: "naoya7076@gmail.com",
       password: "dottle-nouveau-pavilion-tights-furze",
     )
-
-    user.projects.create(
+  end
+  # ユーザー単位では重複したプロジェクト名を許可しないこと
+  it "does not allow duplicate project names per user" do
+    @user.projects.create(
       name: "Test Project"
     )
 
-    new_project = user.projects.build(
+    new_project = @user.projects.build(
       name: "Test Project"
     )
     new_project.valid?
@@ -23,13 +24,7 @@ RSpec.describe Project, type: :model do
 
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Naoya",
-      last_name: "Shimizu",
-      email: "naoya7076@gmail.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-    user.projects.create(
+    @user.projects.create(
       name: "Test Project"
     )
     other_user = User.create(
