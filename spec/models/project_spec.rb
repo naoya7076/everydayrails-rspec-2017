6c721f8655fde327de6bd3a471cf5d7c3d19aff7
naoya7076 <naoya7076@gmail.com>
2020-10-14 09:57:02 +0900

    今日の進捗

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "does not allow duplicate project names per user" do
+  it 'does not allow duplicate project names per user' do
     user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      first_name: 'Joe',
+      last_name:  'Tester',
+      email:      'joetester@example.com',
+      password:   'dottle-nouveau-pavilion-tights-furze',
     )
 
     user.projects.create(
-      name: "Test Project",
+      name: 'Test Project',
     )
 
     new_project = user.projects.build(
-      name: "Test Project",
+      name: 'Test Project',
     )
 
     new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
+    expect(new_project.errors[:name]).to include('has already been taken')
   end
 
-  it "allows two users to share a project name" do
+  it 'allows two users to share a project name' do
     user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      first_name: 'Joe',
+      last_name:  'Tester',
+      email:      'joetester@example.com',
+      password:   'dottle-nouveau-pavilion-tights-furze',
     )
 
     user.projects.create(
