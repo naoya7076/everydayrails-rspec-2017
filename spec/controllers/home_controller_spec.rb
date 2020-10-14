d9585e0fae9aa72ea34466a8eda944b1f872c624
naoya7076 <naoya7076@gmail.com>
2020-10-14 09:41:24 +0900

    コードの修正

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  describe "#index" do
+  describe '#index' do
     # 正常にレスポンスを返すこと
-    it "response successfully" do
+    it 'response successfully' do
       get :index
-      expect(response).to be_success 
+      expect(response).to be_success
     end
 
     # 200レスポンスを返すこと
-    it "returns 200 response" do
+    it 'returns 200 response' do
       get :index
-      expect(response).to have_http_status "200"
+      expect(response).to have_http_status '200'
     end
     
   end
