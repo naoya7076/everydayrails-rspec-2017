e1157315957d3249db792f4421e6f8c429f289cb
naoya7076 <naoya7076@gmail.com>
2020-10-20 22:27:44 +0900

    tasks_controllerのテストを追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# TODO: 5章の演習問題やる
 RSpec.describe TasksController, type: :controller do
   before do
     @user = FactoryBot.create(:user)
@@ -10,10 +11,40 @@ RSpec.describe TasksController, type: :controller do
   describe "#show" do
     it "responds with Json formatted output" do
       sign_in @user
-      get :show, format: :json,
+      get :show, format: :json, # json形式であることを指定する
         params: {project_id: @project.id, id: @task.id}
       expect(response.content_type).to eq "application/json"
     end
   end
-  
+
+  describe "#create" do
+    it "responds with Json formatted output" do
+      new_task = { name: "New test task"}
+      sign_in @user
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task}
+      expect(response.content_type).to eq "application/json"
+    end
+
+    # 新しいタスクをプロジェクトに追加すること
+    it "adds new task to the project" do
+      new_task = { name: "New test task"}
+      sign_in @user
+      expect{
+        post :create, format: :json,
+        params: {project_id: @project.id, task: new_task}
+      }.to change(@project.tasks, :count).by(1)
+    end
+
+    # 認証を要求すること
+    it "requires authentication" do
+      new_task = { name: "New test task"}
+      # ここではログインしない
+      expect{
+        post :create, format: :json,
+        params: {project_id: @project.id, task: new_task}
+      }.to_not change(@project.tasks, :count)
+      expect(response).to_not be_success  
+    end
+  end
 end
