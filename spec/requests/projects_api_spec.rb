bc039da11b5bee8fe4edd4c6aed9bb3d29c76b56
naoya7076 <naoya7076@gmail.com>
2020-10-27 10:00:49 +0900

    リクエストスペックテストをついか

--- spec/requests/projects_api_spec.rb
+++ spec/requests/projects_api_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe "Projects Api", type: :request do
     FactoryBot.create(:project,
       name: "Sample Project")
     FactoryBot.create(:project,
-      name: "Second Sample Project"
+      name: "Second Sample Project",
       owner: user)
 
-    get api_projects_path, params:{
+    get api_projects_path, params: {
       user_email: user.email,
       user_token: user.authentication_token
     }
@@ -21,12 +21,29 @@ RSpec.describe "Projects Api", type: :request do
     project_id = json[0]["id"]
 
     get api_project_path(project_id), params: {
-      user_emai: user.email,
+      user_email: user.email,
       user_token: user.authentication_token
     }
 
-    expect(response).to have_http_status(:success) 
+    expect(response).to have_http_status(:success)
     json = JSON.parse(response.body)
     expect(json["name"]).to eq "Second Sample Project"
+  end
+
+  # プロジェクトを作成できること
+  it 'creates a project' do
+    user = FactoryBot.create(:user)
+
+    project_attributes = FactoryBot.attributes_for(:project)
+
+    expect {
+      post api_projects_path, params: {
+        user_email: user.email,
+        user_token: user.authentication_token,
+        project: project_attributes
+      }
+    }.to change(user.projects, :count).by(1)
+
+    expect(response).to have_http_status(:success) 
   end
 end
