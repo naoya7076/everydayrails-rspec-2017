7e6e092c6ef4fd4f2c9a3a4485d5070bb57c0f9c
naoya7076 <naoya7076@gmail.com>
2020-10-28 08:58:15 +0900

    リクエストスペックの修正

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Projects', type: :request do
       it 'adds a project' do
         project_params = FactoryBot.attributes_for(:project)
         sign_in @user
-        expect { post project_path, params: { project: project_params } }.to change(@user.projects, :count).by(1)
+        expect { post projects_path, params: { project: project_params } }.to change(@user.projects, :count).by(1)
       end
     end
 
